5292cd066322b80fe1710b4377c9bcccc27b9291
Mitchell Michael <mmizzle9@gmail.com>
2013-06-25 09:55:28 -0500

    Added SamsungIap

--- TestApplication/Activity1.cs
+++ TestApplication/Activity1.cs
@@ -6,11 +6,12 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using SamsungChord;
+using SamsungIAP;
 
 namespace TestApplication
 {
     [Activity(Label = "TestApplication", MainLauncher = true, Icon = "@drawable/icon")]
-    public class Activity1 : Activity
+    public class Activity1 : Activity, IAPServiceCallback, IAPConnector
     {
         int count = 1;
 
@@ -28,6 +29,41 @@ namespace TestApplication
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+
+        public Bundle GetItemList(int mode, string packageName, string itemGroupId, int startNum, int endNum, string itemType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bundle GetItemsInbox(string packageName, string itemGroupId, int startNum, int endNum, string startDate, string endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bundle Init(int mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RequestCmd(IAPServiceCallback callback, Bundle bundle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UnregisterCallback(IAPServiceCallback callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBinder AsBinder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResponseCallback(Bundle bundle)
+        {
+            throw new NotImplementedException();
         }
     }
 }
