157385d3423b1c9647915949a550beb7e9113f6d
Mitchell Michael <mmizzle9@gmail.com>
2013-07-12 16:46:29 -0500

    Fixed some interface issues with SamsungIAP
    If and interface and a class share a static name, you have to manage the
    name on the interface and prefix it with an extra 'I'
    interface = "IIAPConnector"
    class = "IAPConnector

--- TestApplication/Activity1.cs
+++ TestApplication/Activity1.cs
@@ -5,7 +5,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-//using SamsungChord;
+using SamsungIAP;
 using Com.Testflightapp.Lib;
 
 namespace TestApplication
