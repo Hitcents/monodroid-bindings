ce96576bad3ffd336084e28a0c381be4f3b6222a
Bryan Phillips <bryan.phillips@hitcents.com>
2015-01-14 11:38:02 -0600

    Adding Chartboost.

--- TestApplication/Activity1.cs
+++ TestApplication/Activity1.cs
@@ -10,7 +10,9 @@ using Android.OS;
 //using Adhub.Ad;
 //using MillennialMedia;
 using Android.Util;
-using MoPub;
+using Com.Chartboost.Sdk;
+using Android.Content.PM;
+//using MoPub;
 //using Com.Facebook.Widget;
 //using Com.Facebook.Android;
 //using Com.Facebook.Internal;
@@ -18,42 +20,97 @@ using MoPub;
 
 namespace TestApplication
 {
-    [Activity(Label = "TestApplication", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "TestApplication", MainLauncher = true, Icon = "@drawable/icon", 
+        ConfigurationChanges = ConfigChanges.Keyboard | ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
     public class Activity1 : Activity
     {
-        private const int IAB_LEADERBOARD_WIDTH = 728,
-            IAB_LEADERBOARD_HEIGHT = 90,
-            MED_BANNER_WIDTH = 480,
-            MED_BANNER_HEIGHT = 60,
-            BANNER_AD_WIDTH = 320, 
-            BANNER_AD_HEIGHT = 50;
         // TODO: Replace this test id with your personal ad unit id
-	    private const string MOPUB_BANNER_AD_UNIT_ID = "d4a0aba637d64a9f9a05a575fa757ac2";
-	    private MoPubView _moPubView;
+        private const string APP_ID = "54744f6a0d602554822b9c6b";
+        private const string APP_SIGNATURE = "2d64505617a06c07579fd8416dbcd3ca08ae13fd";
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
-            var layout = FindViewById<RelativeLayout>(Resource.Id.MMAdLayout);
+            Chartboost.StartWithAppId(this, APP_ID, APP_SIGNATURE);
+            Chartboost.LoggingLevel = Com.Chartboost.Sdk.Libraries.CBLogging.Level.All;
+            Chartboost.SetDelegate(new ChartDelegate());
 
-            _moPubView = (MoPubView)FindViewById<MoPubView>(Resource.Id.adView);
-            _moPubView.AdUnitId = MOPUB_BANNER_AD_UNIT_ID;
-            _moPubView.LoadAd();
+            Chartboost.OnCreate(this);
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            Chartboost.OnStart(this);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Chartboost.OnResume(this);
+            Chartboost.CacheInterstitial(CBLocation.LocationDefault);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            Chartboost.OnPause(this);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            Chartboost.OnStop(this);
         }
 
         protected override void OnDestroy()
         {
-            _moPubView.Destroy();
+            Chartboost.OnDestroy(this);
             base.OnDestroy();
         }
 
-        private bool CanFit(int adWidth)
+        public override void OnBackPressed()
         {
-            var metrics = Resources.DisplayMetrics;
-            var adWidthPx = TypedValue.ApplyDimension(ComplexUnitType.Dip, adWidth, metrics);
-            return metrics.WidthPixels >= adWidthPx;
+            if (Chartboost.OnBackPressed())
+                return;
+            base.OnBackPressed();
+        }
+        
+        internal class ChartDelegate : ChartboostDelegate
+        {
+            public override void DidCloseInterstitial(string p0)
+            {
+                base.DidCloseInterstitial(p0);
+            }
+
+            public override void DidDisplayInterstitial(string p0)
+            {
+                base.DidDisplayInterstitial(p0);
+            }
+
+            public override void DidFailToLoadInterstitial(string p0, Com.Chartboost.Sdk.Model.CBError.CBImpressionError p1)
+            {
+                base.DidFailToLoadInterstitial(p0, p1);
+            }
+
+            public override void DidDismissInterstitial(string p0)
+            {
+                base.DidDismissInterstitial(p0);
+            }
+
+            public override bool ShouldDisplayInterstitial(string p0)
+            {
+                return base.ShouldDisplayInterstitial(p0);
+            }
+
+            public override void DidCacheInterstitial(string p0)
+            {
+                base.DidCacheInterstitial(p0);
+
+                Chartboost.ShowInterstitial(p0);
+            }
         }
     }
 }
